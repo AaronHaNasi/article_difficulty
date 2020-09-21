1a19e0c05a63df86a1b2c7681e21da58b3a21c1b
aaron <kingjojo3@gmail.com>
2020-09-20 20:58:31 -0400

    Changed name of main file to proper name

---
+++ article_difficulty.c
@@ -0,0 +1,12 @@
+// Aaron Bager
+// CIS 343-02
+// Project 1
+
+int main (int argc, char *argv[]) {
+	if ( argc == 0) {
+		printf("Please provide file names"); 
+	}
+	else if ( argc == 1) {
+		
+	}
+}
