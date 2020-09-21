53936ce4ad89476b1f17fb912dd8a5746ef214b3
aaron <kingjojo3@gmail.com>
2020-09-20 21:21:19 -0400

    If one file given checks if exists and returns file name

--- article_difficulty.c
+++ article_difficulty.c
@@ -1,12 +1,22 @@
 // Aaron Bager
 // CIS 343-02
 // Project 1
+#include <unistd.h> // To check if file exists 
+
 
 int main (int argc, char *argv[]) {
-	if ( argc == 0) {
+	if ( argc == 1) {
 		printf("Please provide file names"); 
 	}
-	else if ( argc == 1) {
+	else if ( argc == 2) {
+			if ( access( argv[1], F_OK ) != -1 ) {
+				printf("\nMost difficult document: %s\nLeast difficult document: %s", argv[1], argv[1]); 
+			}
+			else {
+				printf("Error: %s does not exist."); 
+			}
+	}
+	else {
 		
 	}
 }
---
+++ read.c
@@ -0,0 +1,5 @@
+// Aaron Bager
+// CIS 351-02
+// Project 1
+
+#include <unistd.h> // used for the access() function to check if file exists
