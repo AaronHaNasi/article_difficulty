30ebe878990e80d1dd4c46c895ab6bc982281288
Aaron Bager <kingjojo3@gmail.com>
2020-09-24 10:24:45 -0400

    Revert "Cleaned up output"
    This reverts commit 231b54ea8a382edbc38f387f0997f6b0ff30d1c8.

---
+++ article_difficulty.c
@@ -0,0 +1,73 @@
+// Aaron Bager
+// CIS 343-02
+// Project 1
+#include <stdio.h>
+#include <stdlib.h>
+
+double getAverageWordSize(char *fileName) {
+	int wordSize = 0;
+	double average = 0;
+	int numberOfWords = 0;
+	FILE *fp;
+       	char tempChar;
+
+	fp = fopen(fileName, "r"); // open file for reading
+	if (fp == NULL) {
+		printf("%s does not exist. Exiting...", fileName);
+		exit(0);
+	}
+	
+	while( !feof(fp) ) {
+	      tempChar = fgetc(fp);
+		if(tempChar == ' ' || tempChar == '.' || tempChar == '?' || tempChar == '!' || tempChar == ':' || tempChar == ';' || tempChar == ',') {
+			average += wordSize;
+		       	wordSize = 0;
+			numberOfWords++;
+		}
+		else {
+			wordSize++;
+		}
+	}
+	fclose(fp);
+	average = average / numberOfWords;
+	return average;
+}
+
+
+int main (int argc, char **argv) {
+	double smallestWordSize = 10000; // used to keep track of smallest file thus far, intially 1
+	double largestWordSize = 0; // used to keep track of largest file size thus far, initially 0
+	double tempDouble; // used for comparing file sizes
+	int smallestFile; // index of smallest file in arguments given
+	int largestFile; // index number of largest file in arguments given
+	if ( argc == 1) {
+		printf("Please provide file names");
+	}
+	else if ( argc == 2) {
+			FILE *fp; 
+			fp = fopen(argv[1], "r");
+
+			if ( fp != NULL ) {
+				printf("\nMost difficult document: %s\nLeast difficult document: %s", argv[1], argv[1]);
+			}
+			else {
+				printf("Error: %s does not exist.", argv[1]);
+			}
+	}
+	else {
+		for (int i = 1; i < argc; i++) {
+			tempDouble = getAverageWordSize(argv[i]);
+			if (tempDouble > largestWordSize) {
+				largestWordSize = tempDouble;
+				largestFile = i;
+			}
+
+			if (tempDouble < smallestWordSize) {
+				smallestWordSize = tempDouble;
+				smallestFile = i;
+			}
+		}
+
+		printf("Most difficult document: %s\nLeast difficult document: %s", argv[largestFile], argv[smallestFile]);
+	}
+}
